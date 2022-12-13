e98a55ef29552c7a864d56fe2b1702b03301de1d
DenisYancevich <116888805+DenisYancevich@users.noreply.github.com>
2022-12-13 03:10:34 +0200

    Add files via upload

---
+++ DZ-2/main.js
@@ -0,0 +1,9 @@
+const hours = prompt("What time is it? (hours)");
+
+const seconds = hours * 60 * 60;
+
+alert(`There is ${numberWithSpaces(seconds)} second/s in ${hours} hour/s`);
+
+function numberWithSpaces(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
