14ed1843c92dcda0e79d7922c48f94c35560e698
DenisYancevich <116888805+DenisYancevich@users.noreply.github.com>
2022-12-13 03:06:57 +0200

    Add files via upload

---
+++ DZ-1/main.js
@@ -0,0 +1,8 @@
+const a = prompt("Input first number");
+const b = prompt("Input second number");
+
+alert(
+  `${a} + ${b} = ${+a + +b}\n${a} - ${b} = ${a - b}\n${a} * ${b} = ${
+    a * b
+  }\n${a} / ${b} = ${a / b}`
+);
