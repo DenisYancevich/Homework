81e42b3a49d641df243b12900dec0b5e95a9a425
DenisYancevich <116888805+DenisYancevich@users.noreply.github.com>
2022-12-29 01:00:05 +0200

    Add files via upload

---
+++ DZ-12/main.js
@@ -0,0 +1,18 @@
+function removeElement(arr, item) {
+  if (!Array.isArray(arr)) {
+    console.log("Given item isn`t an Array!");
+  } else {
+    const index = arr.indexOf(item);
+    if (index !== -1) {
+      arr.splice(index, 1);
+    } else {
+      console.log("Given item not in Array!");
+    }
+  }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(array);
+removeElement(array, 2);
+console.log(array);
