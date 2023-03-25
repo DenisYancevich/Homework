2b5db123e21760d11511779bc08797ce6f735839
DenisYancevich <116888805+DenisYancevich@users.noreply.github.com>
2023-03-25 15:12:48 +0200

    Add files via upload

---
+++ DZ-38/apiCall.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+async function postWeather(city) {
+  return axios
+    .post(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city.toUpperCase()}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+    )
+    .catch((data) => data);
+}
+
+export default postWeather;
