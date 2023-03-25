7cd400b5030b5969565dc1cd37fc8a839c90ca82
DenisYancevich <116888805+DenisYancevich@users.noreply.github.com>
2023-03-25 15:19:50 +0200

    Add files via upload

---
+++ DZ-39/my-app/src/components/mainContainer.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class MainContainer extends Component {
+  state = {
+    content: [],
+  };
+  render() {
+    return <div className="mainContainer">{this.props.content}</div>;
+  }
+}
+
+export default MainContainer;
